19031e842d00197b5ecad309c7a3fc2b3f7ef11f
lvyizhuo <lvonezhuo@163.com>
2026-09-20 23:33:25 +0800

    feat: 新增 LeetCode 283. 移动零 题解及相关文件

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-str = 'eat'
-print(sorted(str))  # ['a', 'e', 't'] —— sorted() 返回排序后的列表，原字符串不动
-key = ''.join(sorted(str))  # ''.join() 把列表拼成字符串
-print(key)  # 'aet' —— 排序后的字符串作为哈希表的
-
-hashmap = {key:1 ,'name':'lvyzihzuo' }
-print (list((hashmap.values())))
+s = "eat"
+print(sorted(s))  # ['a', 'e', 't']
+print(type(sorted(s)))  # <class 'list'>
