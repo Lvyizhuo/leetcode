e16355f4b780c091c9acbec57a8edcbe0b7d0f36
lvyizhuo <lvonezhuo@163.com>
2026-09-21 22:48:36 +0800

    feat: 新增 LeetCode 11. 盛最多水的容器题解及相关文件

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-s = "eat"
-print(sorted(s))  # ['a', 'e', 't']
-print(type(sorted(s)))  # <class 'list'>
+nums = [1,2,3,4,5]
+# print(len(nums))  # 输出 5
+for i in range(len(nums)):
+    print(nums[i])
